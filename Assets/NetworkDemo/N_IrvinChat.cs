0c20d421668449aa015ce182d3554f651eaf1606
NeoTail <61777230+NeoTail@users.noreply.github.com>
2021-02-12 17:03:59 +0100

    JPP PLAYER

--- Assets/NetworkDemo/N_IrvinChat.cs
+++ Assets/NetworkDemo/N_IrvinChat.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class N_IrvinChat : MonoBehaviour
 {
-
+    [SerializeField] N_Player player = null;
+    public void OnGetMessages(string _channelName, string[] _senders, string[] _messages, Color _color)
+    {
+        string _msgs = "";
+        for (int i = 0; i < _senders.Length; i++)
+            _msgs = string.Format($" {_msgs} {_senders} : {_messages}, ");
+        //Console.WriteLine($"Messages: {_channelName} ({_senders.Length}) > {_msgs}");
+    }
 }
