f710b4bedd8d0adbc54741d70b71b3c06d7ece3b
NeoTail <61777230+NeoTail@users.noreply.github.com>
2021-02-12 16:38:15 +0100

    Update N_Player.cs

--- Assets/NetworkDemo/N_Player.cs
+++ Assets/NetworkDemo/N_Player.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using Photon.Pun;
 using System;
 using TMPro;
@@ -7,35 +9,66 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class N_Player : MonoBehaviour
 {
-	public event Action OnUpdatePlayer = null;
-	[SerializeField] PhotonView photonID = null;
-	[SerializeField] TMP_Text nameLabel = null;
-	Renderer playerColor = null;
-	public bool IsValid => photonID && playerColor && nameLabel;
+    public event Action OnUpdatePlayer = null;
 
-	void Start()
-	{
-		InitPlayer();
-	}
+    [SerializeField] PhotonView photonID = null;
+    [SerializeField] TextMeshPro nameLabel = null;
+    [SerializeField] Renderer playerColor = null;
+    Vector3 localColor = Vector3.zero, onlineColor = Vector3.zero;
 
-	void InitPlayer()
-	{
-		photonID = PhotonView.Get(this);
-		playerColor = GetComponent<Renderer>();
-		if (!IsValid) return;
-		nameLabel.text = photonID.Owner.NickName;
-		SetNetworkColor();
-	}
+    public bool IsValid => photonID && nameLabel && playerColor;
 
-	void SetNetworkColor()
-	{
-		if (!IsValid) return;
-		Color _color = Random.ColorHSV();
-		if (photonID.IsMine)
-			if (photonID.IsColor()) _color = photonID.GetColorOnline();
-			else photonID.SetColorOnline(_color);
-		else _color = photonID.GetColorOnline();
-		playerColor.material.color = _color;
-	}
+    private void Start()
+    {
+        InitPlayer();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && photonID.IsMine)
+            SetPlayerNewColor(GetRandomColor());
+    }
+    public void InitPlayer()
+    {
+        photonID = PhotonView.Get(this);
+        if (!playerColor) playerColor = GetComponent<Renderer>();
+        if (!IsValid) return;
+        nameLabel.text = photonID.Owner.NickName;
+        SetNetworkColor();
+    }
+
+    void SetNetworkColor()
+    {
+        if (!IsValid) return;
+        Color _color = Random.ColorHSV();
+        if (photonID.IsMine)
+        {
+            if (photonID.IsColor())
+                _color = photonID.GetColorOnline();
+            else
+                photonID.SetColorOnline(_color);
+        }
+        else
+            _color = photonID.GetColorOnline();
+        playerColor.material.color = _color;
+
+    }
+
+
+    Color GetRandomColor() => Random.ColorHSV();
+
+    void SetPlayerNewColor(Color _color)
+    {
+        if (!IsValid) return;
+        playerColor.material.color = _color;
+        if (!photonID.IsMine) return;
+        photonID.RPC("SetPlayerNewColorOnline", RpcTarget.All, new float[] {_color[0], _color[1], _color[2]});
+    }
+
+    [PunRPC]
+    void SetPlayerNewColorOnline(float[] _color)
+    {
+        if (!playerColor) return;
+        playerColor.material.color = new Color(_color[0], _color[1], _color[2]);
+    }
 }
-
