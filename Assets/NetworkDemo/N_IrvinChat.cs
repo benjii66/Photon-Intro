a0ff10b154d177df7ed9a418ad948b3f8f2b5efb
NeoTail <61777230+NeoTail@users.noreply.github.com>
2021-02-15 16:58:33 +0100

    Begin Irvin Chat

--- Assets/NetworkDemo/N_IrvinChat.cs
+++ Assets/NetworkDemo/N_IrvinChat.cs
@@ -1,15 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
-public class N_IrvinChat : MonoBehaviour
+public class N_IrvinChat : MonoBehaviour, IPunObservable
 {
-    [SerializeField] N_Player player = null;
-    public void OnGetMessages(string _channelName, string[] _senders, string[] _messages, Color _color)
+    public event Action OnUpdateChat = null;
+    [SerializeField] TMP_InputField inputField = null;
+    [SerializeField] Button sendButton = null;
+    [SerializeField] PhotonView photonID = null;
+    public bool IsValid => photonID && inputField && sendButton;
+
+    void Start()
     {
-        string _msgs = "";
-        for (int i = 0; i < _senders.Length; i++)
-            _msgs = string.Format($" {_msgs} {_senders} : {_messages}, ");
-        //Console.WriteLine($"Messages: {_channelName} ({_senders.Length}) > {_msgs}");
+        InitNetworkObject();
+        if (!IsValid) return;
+        if (photonID.IsMine) OnUpdateChat += UpdateLocal;
+        else OnUpdateChat += UpdateOnline;
+
+    }
+
+    void Update() => OnUpdateChat?.Invoke();
+
+    void SendChatMessage()
+    {
+
+    }
+
+    void UpdateLocal()
+    {
+        
+    }
+
+    void UpdateOnline()
+    {
+
+    }
+
+    void InitNetworkObject()
+    {
+        photonID = PhotonView.Get(this);
+    }
+
+    void WriteOnlineVector(bool _canWrite, PhotonStream _stream, Vector3 _vector)
+    {
+        if (!_canWrite) return;
+        if (_stream.IsWriting) _stream.Serialize(ref _vector);
+    }
+
+    Vector3 ReadOnlineVector(PhotonStream _stream)
+    {
+        if (_stream.IsReading)
+            return (Vector3)_stream.ReceiveNext();
+        else return Vector3.zero;
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+
     }
 }
