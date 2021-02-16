d58f0b859bb99902416ad62658109e5a7777297b
NeoTail <61777230+NeoTail@users.noreply.github.com>
2021-02-16 10:43:42 +0100

    Correc Ir20

---
+++ Assets/NetworkDemo/ICorrection/ICM_Chat.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Photon.Pun;
+
+[Serializable]
+public class ICM_Chat : MonoBehaviour
+{
+    [SerializeField] PhotonView chatNetOwner = null;
+    [SerializeField] List<ICM_ChatMessage> chatConversation = new List<ICM_ChatMessage>();
+    public List<ICM_ChatMessage> ChatConversation => chatConversation;
+    public string CurrentLine { get; set; } = "";
+    public void Init(PhotonView _owner) => chatNetOwner = _owner;
+
+    public void SendChatMessage()
+    {
+        Send();
+    }
+
+    void Send()
+    {
+        ICM_ChatMessage _msg = new ICM_ChatMessage(PhotonNetwork.LocalPlayer.NickName, CurrentLine, GetChatColor());
+        chatConversation.Add(_msg);
+        CurrentLine = "";
+        if (!chatNetOwner) return;
+        chatNetOwner.RPC("SendLocalMessage", RpcTarget.Others, JsonUtility.ToJson(_msg));
+    }
+
+    [PunRPC]
+    void SendOnlineMessage(string _objectData)
+    {
+        ICM_ChatMessage _msg = JsonUtility.FromJson<ICM_ChatMessage>(_objectData);
+        chatConversation.Add(_msg);
+    }
+
+    string GetChatColor()
+    {
+        if (!PhotonNetwork.IsConnected) return $"#{ColorUtility.ToHtmlStringRGB(Color.white)}";
+        float[] _colorFloat = (float[])PhotonNetwork.LocalPlayer.CustomProperties["color"];
+        Color _color = new Color(_colorFloat[0], _colorFloat[1], _colorFloat[2]);
+        return $"#{ColorUtility.ToHtmlStringRGB(_color)}";
+    }
+}
+
+[Serializable]
+public struct ICM_ChatMessage
+{
+    [SerializeField] string ownerNickname;
+    [SerializeField] string message;
+    [SerializeField] string date;
+    [SerializeField] string colorData;
+
+    public string OwnerNickname => ownerNickname;
+    public string Message => message;
+    public string CompleteMessage => $"<color={colorData}><b> {ownerNickname} - {date} : {message}</b></color>";
+
+    public ICM_ChatMessage(string _owner, string _message, string _colorData)
+    {
+        ownerNickname = _owner;
+        message = _message;
+        date = System.DateTime.Now.ToString();
+        colorData = _colorData;
+    }
+}
---
+++ Assets/NetworkDemo/ICorrection/ICM_ChatManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ICM_ChatManager : MonoBehaviour
+{
+    [SerializeField] PhotonView chatNetID = null;
+    [SerializeField] ICM_Chat chat = null;
+    public bool IsValid => chatNetID;
+
+    private void Start()
+    {
+        InitChat();
+    }
+
+    void InitChat()
+    {
+        chatNetID = PhotonView.Get(this);
+        if (!IsValid) return;
+        chat = new ICM_Chat();
+    }
+
+    void OnGUI()
+    {
+        if (!PhotonNetwork.IsConnected) return;
+        Rect _windowRect = new Rect(0, Screen.height - Screen.height * .5f, Screen.width * .5f, Screen.height * .5f);
+        GUILayout.Window(0, _windowRect, ChatWindow, "Chat");
+    }
+
+    void ChatWindow(int _id)
+    {
+        GUILayout.BeginHorizontal();
+        chat.CurrentLine = GUILayout.TextArea(chat.CurrentLine);
+        if(GUILayout.Button("Send")) chat?.SendChatMessage();
+        GUILayout.EndHorizontal();
+
+        for (int i = 0; i < chat.ChatConversation.Count; i++)
+            GUILayout.Box(chat.ChatConversation[i].CompleteMessage, GetChatStyle());
+    }
+
+    GUIStyle GetChatStyle()
+    {
+        GUIStyle _chat = GUI.skin.box;
+        _chat.richText = true;
+        return _chat;
+    }
+}
